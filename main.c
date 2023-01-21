f53c64c2c55b32a77d21047899ab7885a277381e
KOPE <kittisak.kope2021@gmail.com>
2023-01-21 21:51:01 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define str "This is s the string variable"
+
+int main()
+{
+    int age = 20;
+    int month = 5;
+    int year = 2001;
+    int day = 12;
+    int hello;
+    hello = year - age;
+    char Fjike = 'A';
+
+    printf("The value of age is %d and the today is %d \n", age, day);
+    printf("The result of hello is %d \n", hello);
+    printf("The new variable is %s", str);
+
+    return 0;
+}
